27ac1d1a4f9659358b88cb0b9e1e1a492a0ee089
Tintouin <82973239+Tintouin@users.noreply.github.com>
2026-06-02 22:32:08 +0200

    making images work hopefully

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,16 +1,17 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy({ "main.css": "main.css" });
-    eleventyConfig.addPassthroughCopy({ "fonts.css": "fonts.css" });
-    eleventyConfig.addPassthroughCopy({ "scripts": "scripts" });
-    eleventyConfig.addPassthroughCopy({ "images": "images" });
-    eleventyConfig.addPassthroughCopy({ "fonts": "fonts" });
-    eleventyConfig.addPassthroughCopy({ "comics": "comics" });
-  
-    return {
-      dir: {
-        input: "src",
-        output: "_site",
-        includes: "_includes"
-      }
-    };
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "main.css": "main.css" });
+  eleventyConfig.addPassthroughCopy({ "fonts.css": "fonts.css" });
+  eleventyConfig.addPassthroughCopy({ scripts: "scripts" });
+  eleventyConfig.addPassthroughCopy({ images: "images" });
+  eleventyConfig.addPassthroughCopy({ fonts: "fonts" });
+  eleventyConfig.addPassthroughCopy({ comics: "comics" });
+
+  return {
+    pathPrefix: "/",
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+    },
   };
+};
